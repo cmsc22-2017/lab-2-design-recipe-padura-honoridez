195af7edbc76d582510b8f97e2ad37e37d3c915b
Joshua Ven Honoridez <31608108+JoshuaVen@users.noreply.github.com>
2017-09-11 14:44:49 +0800

    Update Pets.java

--- Pets.java
+++ Pets.java
@@ -70,7 +70,7 @@ class Dog implements IPet {
 
 class ExampleIPetandPerson {
 	ExampleIPetandPerson() {}
-	
+
 	IPet persian = new Cat("Cathy", "Persian", true);
 	IPet sphynx = new Cat("Luke", "Sphynx", false);
 	IPet collie = new Dog("Whitie", "Border Collie", true);
