47347ec36ba47c40236e2f1b3a4d59356ce21db2
Joshua Ven Honoridez <31608108+JoshuaVen@users.noreply.github.com>
2017-09-11 14:43:39 +0800

    Add files via upload

--- Pets.java
+++ Pets.java
@@ -1,19 +1,46 @@
 import tester.Tester;
 
+//to represent a pet
+interface IPet { }
+
+
 // to represent a pet owner
-class Person {
+class Human {
   String name;
   IPet pet;
   int age;
 
-  Person(String name, IPet pet, int age) {
+  Human(String name, IPet pet, int age) {
     this.name = name;
     this.pet = pet;
     this.age = age;
   }
+  
+  /* TEMPLATE
+   * Fields:
+   * ... this.name ...          -- String
+   * ... this.age ...           -- int
+   * ... this.pet ...           -- IPet
+   * 
+   * Methods:
+   * ... this.isOlder(Human) ...       -- boolean
+   * 
+   * Fields of Parameter:
+   * ... this.name ...          -- String
+   * ... this.age ...           -- int
+   * ... this.pet ...           -- IPet
+   * 
+   * Methods on Parameters:
+   * ... this.isOlder(Human) ...       -- boolean
+   */
+  
+  // -> boolean
+  // Returns true if one human, compared with another human, is older.
+  boolean isOlder(Human that) {
+    return this.age > that.age;
+  }
 }
-// to represent a pet
-interface IPet { }
+
 
 // to represent a pet cat
 class Cat implements IPet {
@@ -41,24 +68,29 @@ class Dog implements IPet {
   }
 }
 
-class ExampleIPet {
-	ExampleIPet() {}
+class ExampleIPetandPerson {
+	ExampleIPetandPerson() {}
 	
 	IPet persian = new Cat("Cathy", "Persian", true);
 	IPet sphynx = new Cat("Luke", "Sphynx", false);
 	IPet collie = new Dog("Whitie", "Border Collie", true);
 	IPet doberman = new Dog("Percy", "Doberman Pincher", false);
+	
+	Human jack = new Human("Jack", collie, 31);
+	Human drew = new Human("Drew", doberman, 15);
+  Human luna = new Human("Luna", persian, 20);
+  Human rachel = new Human("Rachel", sphynx, 27);
+  Human jill = new Human("Jill", collie, 31);
+  Human ann = new Human("Ann", doberman, 10);
+  Human ranger = new Human("Ranger", persian, 19);
+  Human nicole = new Human("Nicole", sphynx, 30);
+	
+  boolean testIsOlder(Tester t) {
+   return t.checkExpect(nicole.isOlder(rachel), true) && 
+          t.checkExpect(jill.isOlder(jack), false) &&
+          t.checkExpect(drew.isOlder(ann), true) &&
+          t.checkExpect(luna.isOlder(rachel), false);
+  }
 }
 
-class ExamplePerson {
-	ExamplePerson() {}
-	
-	Person jack = new Person("Jack", collie, 31);
-	Person drew = new Person("Drew", doberman, 15);
-	Person luna = new Person("Luna", persian, 20);
-	Person rachel = new Person("Rachel", sphynx, 27);
-	Person jill = new Person("Jill", collie, 32);
-	Person ann = new Person("Ann", doberman, 10);
-	Person ranger = new Person("Ranger", persian, 19);
-	Person nicole = new Person("Nicole", sphynx, 30);
-}
+
